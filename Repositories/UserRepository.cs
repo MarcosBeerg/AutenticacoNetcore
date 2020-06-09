4f3cb128fc49fbfb9ead0c7891b95f466ffd2022
Your Name <you@example.com>
2020-06-09 19:42:44 -0300

    AutenticacaoNetCore

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using Shop.Models;
+
+namespace Shop.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "berg", Password = "berg", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "limao", Password = "limao", Role = "employee" });
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
